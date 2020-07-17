0e81858f97af9c6a9225fc011880f8affb84adbf
braum <braumdrave@gmail.com>
2020-07-17 19:55:28 +0300

    Without filter js and nav js

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// window.addEventListener('scroll', function () {
+//
+// })
+
+
+
+$(document).ready(function(){
+    $("#teamCarousel").owlCarousel({
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            450: {
+              items: 2
+            },
+            780: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    });
+    $("#clientsContainer").owlCarousel({
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            450: {
+                items: 2
+            },
+            750: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            }
+        }
+    });
+    $("#testimonialsCarousel").owlCarousel({
+        items: 1
+    });
+});
